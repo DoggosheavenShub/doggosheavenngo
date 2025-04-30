cf2a677863551d9d195837d593a0ac2465c7f42e
Gagandeep-Sethi <107262250+Gagandeep-Sethi@users.noreply.github.com>
2025-04-30 17:52:15 +0530

    fixed cors origin

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ console.log(process.env.FRONTEND_URL);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: origin: ["https://doggosheavenngo-frontend.vercel.app","https://www.doggosheaven.org"],
     allowedHeaders: ["Content-Type", "Accept-Type"],
     methods: ["GET", "POST"],
   })
